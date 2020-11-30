a070d44c63c5e54c1fb1bab333df76e8f0355fa9
mandy0315 <41838993+mandy0315@users.noreply.github.com>
2020-11-30 11:38:42 +0800

    test

--- js/jquery.pagepiling.js
+++ js/jquery.pagepiling.js
@@ -633,33 +633,7 @@
         * Determines the way of scrolling up or down:
         * by 'automatically' scrolling a section or by using the default and normal scrolling.
         */
-        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-            $(document).on('swipeleft', '.ui-page', function (event) {
-                if (event.handled !== true) // This will prevent event triggering more then once
-                {
-                    var nextpage = $.mobile.activePage.next('[data-role="page"]');
-                    // swipe using id of next page if exists
-                    if (nextpage.length > 0) {
-                        $.mobile.changePage(nextpage, { transition: "slide", reverse: false }, true, true);
-                    }
-                    event.handled = true;
-                }
-                return false;
-            });
-
-            $(document).on('swiperight', '.ui-page', function (event) {
-                if (event.handled !== true) // This will prevent event triggering more then once
-                {
-                    var prevpage = $(this).prev('[data-role="page"]');
-                    if (prevpage.length > 0) {
-                        $.mobile.changePage(prevpage, { transition: "slide", reverse: true }, true, true);
-                    }
-                    event.handled = true;
-                }
-                return false;
-            });
-
-        } else {
+        
             function scrolling(type, scrollable) {
                 var check;
                 var scrollSection;
@@ -683,8 +657,6 @@
                     scrollSection();
                 }
             }
-
-        }
         
         /**
         * Return a boolean depending on whether the scrollable element is at the end or at the start of the scrolling
