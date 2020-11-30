60d90d4760b468a901bd5c7704983dc84856c4f1
mandy0315 <41838993+mandy0315@users.noreply.github.com>
2020-11-30 22:44:55 +0800

    test

--- js/loading.js
+++ js/loading.js
@@ -1,27 +1,21 @@
-$(document).ready(function () {
+$(document).ready(function(){
   if($("body").hasClass('home')){
     LoadingInit();
   }
-window.onload = function() {
-    if($("body").hasClass('home')){
-      alert("測試1");
-      LoadingClose();
-    }
-}
+  window.onload = function(){
+      if($("body").hasClass('home')){
+        alert("測試1");
+        LoadingClose();
+      }
+  }
   function LoadingInit(){
     $('#loading-box').load("./share/loading.html");
-    // setTimeout(() => {
-    //   $(".loading-logo").addClass('active2')
-    // }, 1500);
     setTimeout(() => {
       $("#loading").show
     }, 2500);
   }
-  function LoadingClose() {
+  function LoadingClose(){
     $('#loading-box').load("./share/loading.html");
-    // setTimeout(() => {
-    //   $(".loading-logo").addClass('active2')
-    // }, 1500);
     setTimeout(() => {
       alert("測試2");
       $("#loading-box").slideUp(500);
