977a2bad99470cd57eb4fc31da0f35de7d9d07a1
mandy0315 <41838993+mandy0315@users.noreply.github.com>
2021-01-05 09:40:59 +0800

    test18

--- js/all.js
+++ js/all.js
@@ -93,10 +93,10 @@ $(document).ready(function () {
     //     })
     // }
     //Mobile Chrome doesn't work the normal way, so... lets use HTML5 for phones :) 
-    if (isTouchDevice || isTouch) { 
-        history.replaceState(undefined, undefined, '#' + url); 
-    } else { 
-        var baseUrl = window.location.href.split('#')[0]; 
-        window.location.replace(baseUrl + '#' + url); 
-    } 
+    // if (isTouchDevice || isTouch) { 
+    //     history.replaceState(undefined, undefined, '#' + url); 
+    // } else { 
+    //     var baseUrl = window.location.href.split('#')[0]; 
+    //     window.location.replace(baseUrl + '#' + url); 
+    // } 
 });
