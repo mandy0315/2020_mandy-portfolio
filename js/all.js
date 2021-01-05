a12af7948030dcd53a62249af2b49c2e5d967ce3
mandy0315 <41838993+mandy0315@users.noreply.github.com>
2021-01-05 09:38:08 +0800

    test18

--- js/all.js
+++ js/all.js
@@ -92,4 +92,11 @@ $(document).ready(function () {
     //         hashNum = data2[hashName];
     //     })
     // }
+    //Mobile Chrome doesn't work the normal way, so... lets use HTML5 for phones :) 
+    if (isTouchDevice || isTouch) { 
+        history.replaceState(undefined, undefined, '#' + url); 
+    } else { 
+        var baseUrl = window.location.href.split('#')[0]; 
+        window.location.replace(baseUrl + '#' + url); 
+    } 
 });
