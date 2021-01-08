2054a3b492e228873fe8e41bbbacdbb1e6bc67d8
mandy0315 <41838993+mandy0315@users.noreply.github.com>
2021-01-08 16:16:54 +0800

    update-210108

--- js/loading.js
+++ js/loading.js
@@ -1,22 +1,33 @@
 $(document).ready(function(){
-  if($("body").hasClass('container')){
-    LoadingInit();
-  }
-  function LoadingInit(){
-    $('#loading-box').load("./share/loading.html");
-    $('#loading-works-box').load(".././share/loading.html");
-    // setTimeout(() => {
-    //   $("#loading").show
-    // }, 2500);
-  }
-});
-$(window).on('load',function () {
-  if ($("body").hasClass('container')) {
-    LoadingClose();
-  }
-  function LoadingClose() {
-  setTimeout(() => {
-    $("#loading-box,#loading-works-box").slideUp(500);
-  }, 1000);
-  }
+	const width = 100
+	const perfData = window.performance.timing //检测页面加载速度
+	let EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart)//loadEventEnd load事件執行完的時間/navigationStart 加載起始時間
+	let time = parseInt((EstimatedTime/1000)%60)*100
+	
+	if ($("body").hasClass('container')) {
+		$('#loading-wrapper').load('./share/loading.html', function () {
+			$('#loading-map').css('display', 'flex');
+			LoadingInit();
+		});
+		$('#m-loading-wrapper').load('.././share/loading.html', function () {
+			$('#loading-map').css('display', 'flex');
+			LoadingInit();
+		});
+		LoadingInit();
+		LoadingClose();
+	}
+	function LoadingInit(){
+		// Loadbar Animation 
+		$(".loadbar").animate({
+			width: width + "%"
+		}, time);
+		$(".loading-my").animate({
+			marginLeft: 100 + "%"
+		}, time);
+	}
+	function LoadingClose() {
+		setTimeout(() =>{
+			$('#loading-container').fadeOut(400);
+		}, time);
+	}
 });
