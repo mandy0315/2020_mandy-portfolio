a90f1f5ab185b7739d55492045f7b0ea928fcb10
Mandy Tsai <mandy3152618@gmail.com>
2022-05-16 22:31:56 +0800

    fix: 修改 js 壓縮檔

--- js/workspages.js
+++ js/workspages.js
@@ -1,1 +1,19 @@
-$(document).ready(function(){$('#scroll-to-top').click(function(){return $('html, body').animate({scrollTop:0},400),!1});let a=window.matchMedia("(max-width: 768px)");b(a);function b(a){a.matches?$(".worksPages-text-wrapper").removeClass("scrollme"):$(".worksPages-text-wrapper").addClass("scrollme")}})
+$(document).ready(function () {
+    // 按下執行 scroll-to-top 
+    $('#scroll-to-top').click(function() {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 400);
+		return false;
+	});
+	// 768px 執行滾動
+    let mm = window.matchMedia("(max-width: 768px)");
+    resizeWidth(mm);
+    function resizeWidth(pMatchMedia) {
+        if (pMatchMedia.matches) {
+            $(".worksPages-text-wrapper").removeClass("scrollme");
+        }else {
+			$(".worksPages-text-wrapper").addClass("scrollme");
+        }
+    }
+});
